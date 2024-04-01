e6673087839b4c2e6e7c7883e0cf563c93f8d427
Karan Trehan <karan.trehan@springworks.in>
2024-04-02 00:55:54 +0530

    Recursion | Print subsequences whose sum is K

---
+++ src/recursion/9_print_subsequences_with_sum_k/index.js
@@ -0,0 +1,40 @@
+/**
+ * Prints all the subsequences of an array with a given sum.
+ *
+ * @param {number} index - The current index of the array.
+ * @param {Array<number>} arr - The current subsequence being formed.
+ * @param {number} currentSum - The current sum of the subsequence.
+ * @param {number} sum - The target sum.
+ * @param {Array<number>} array - The input array.
+ * @returns {void}
+ */
+function printSubsequence(index, arr, currentSum, sum, array) {
+  if (index === array.length) {
+    // base-case
+    if (currentSum === sum) {
+      console.log(arr);
+    }
+    return;
+  }
+
+  arr.push(array[index]);
+  currentSum += array[index];
+
+  printSubsequence(index + 1, arr, currentSum, sum, array); // pick
+
+  arr.pop();
+  currentSum -= array[index];
+
+  printSubsequence(index + 1, arr, currentSum, sum, array); // not-pick
+
+  return;
+}
+
+/**
+ * index = 0
+ * arr = []
+ * currentSum = 0
+ * sum = 2 (target sum)
+ * array = [1, 2, 1]
+ */
+printSubsequence(0, [], 0, 2, [1, 2, 1]); // invoking call
