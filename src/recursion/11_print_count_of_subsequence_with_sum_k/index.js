aca7b30e4a68ad8a0d279a94763e07d453f0cf1a
Karan Trehan <karan.trehan@springworks.in>
2024-04-02 01:06:22 +0530

    Recursion | Count subsequences whose sum is k

--- src/recursion/11_print_count_of_subsequence_with_sum_k/index.js
+++ src/recursion/11_print_count_of_subsequence_with_sum_k/index.js
@@ -1,52 +1,50 @@
 /**
- * Prints all the subsequences of an array with a given sum.
+ * Returns 1 if a subsequence with a given sum is found, otherwise 0.
+ * Recursively finds the count of subsequences whose sum results in the target sum.
  *
  * Time Complexity: O(2^n)
  * Space Complexity: O(n)
  *
  * @param {number} index - The current index of the array.
- * @param {Array<number>} arr - The current subsequence being formed.
  * @param {number} currentSum - The current sum of the subsequence.
  * @param {number} sum - The target sum.
  * @param {Array<number>} array - The input array.
- * @returns {void}
+ * @returns {number}
  */
-function printSubsequences(index, arr, currentSum, sum, array) {
+function countSubsequence(index, currentSum, sum, array) {
   if (index === array.length) {
     // base-case
     if (currentSum === sum) {
-      console.log(arr);
+      return 1;
     }
-    return;
+    return 0;
   }
 
-  arr.push(array[index]);
   currentSum += array[index];
 
-  printSubsequences(index + 1, arr, currentSum, sum, array); // pick
+  const left = countSubsequence(index + 1, currentSum, sum, array); // pick
 
-  arr.pop();
   currentSum -= array[index];
 
-  printSubsequences(index + 1, arr, currentSum, sum, array); // not-pick
+  // not-pick
+  const right = countSubsequence(index + 1, currentSum, sum, array); // pick
 
-  return;
+  return left + right;
 }
 
 /**
  * Input:
  *
  * index = 0
- * arr = []
  * currentSum = 0
  * sum = 2 (target sum)
  * array = [1, 2, 1]
  */
-printSubsequences(0, [], 0, 2, [1, 2, 1]); // invoking call
+const count = countSubsequence(0, 0, 2, [1, 2, 1]); // invoking call
+console.log(count);
 
 /**
  * Output:
  *
- * [1, 1]
- * [2]
+ * 2
  */
