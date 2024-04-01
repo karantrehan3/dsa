7f3df38e891d003a2c149d38cc19c7ce75ecae2a
Karan Trehan <karan.trehan@springworks.in>
2024-04-02 01:00:00 +0530

    Recursion | Print 1 subsequence whose sum is k

--- src/recursion/10_print_1_subsequence_with_sum_k copy/index.js
+++ src/recursion/10_print_1_subsequence_with_sum_k copy/index.js
@@ -1,33 +1,40 @@
 /**
- * Prints all the subsequences of an array with a given sum.
+ * Prints the first subsequence of an array with a given sum.
  *
  * @param {number} index - The current index of the array.
  * @param {Array<number>} arr - The current subsequence being formed.
  * @param {number} currentSum - The current sum of the subsequence.
  * @param {number} sum - The target sum.
  * @param {Array<number>} array - The input array.
- * @returns {void}
+ * @returns {boolean}
  */
 function printSubsequence(index, arr, currentSum, sum, array) {
   if (index === array.length) {
     // base-case
     if (currentSum === sum) {
       console.log(arr);
+      return true;
     }
-    return;
+    return false;
   }
 
   arr.push(array[index]);
   currentSum += array[index];
 
-  printSubsequence(index + 1, arr, currentSum, sum, array); // pick
+  // pick
+  if (printSubsequence(index + 1, arr, currentSum, sum, array)) {
+    return true;
+  }
 
   arr.pop();
   currentSum -= array[index];
 
-  printSubsequence(index + 1, arr, currentSum, sum, array); // not-pick
+  // not-pick
+  if (printSubsequence(index + 1, arr, currentSum, sum, array)) {
+    return true;
+  }
 
-  return;
+  return false;
 }
 
 /**
@@ -45,5 +52,4 @@ printSubsequence(0, [], 0, 2, [1, 2, 1]); // invoking call
  * Output:
  * 
  * [1, 1]
- * [2]
  */
