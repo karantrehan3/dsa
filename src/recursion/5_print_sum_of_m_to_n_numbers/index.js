f5f1e08b7b1f113793e32c890e7178c7182f85bf
Karan Trehan <karan.trehan@springworks.in>
2024-04-02 00:37:25 +0530

    Recursion | Sum of numbers from m to n

---
+++ src/recursion/5_print_sum_of_m_to_n_numbers/index.js
@@ -0,0 +1,22 @@
+/**
+ * Prints the sum of numbers from m to n recursively.
+ *
+ * Time Complexity: O(n) (depth of the recursion tree)
+ * Space Complexity: O(n)
+ * @param {number} m - The starting number
+ * @param {number} n - The ending number
+ * @returns {number}
+ */
+function sumNumbers(m, n) {
+    if (m > n) {
+      // base-case
+      return 0;
+    }
+
+    return m + sumNumbers(m + 1, n); // recursive-call;
+  }
+  
+  // invoking call
+  const sum = sumNumbers(1, 5);
+  console.log(sum);
+  
